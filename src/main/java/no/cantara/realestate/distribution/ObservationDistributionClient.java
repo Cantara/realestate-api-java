4021270218d6d998a9dfd62363f26d0215e7a0b7
baardl <bard.lind@gmail.com>
2026-06-29 10:34:46 +0200

    Adde Exception instructions to interface

--- src/main/java/no/cantara/realestate/distribution/ObservationDistributionClient.java
+++ src/main/java/no/cantara/realestate/distribution/ObservationDistributionClient.java
@@ -1,9 +1,12 @@
 package no.cantara.realestate.distribution;
 
 
+import no.cantara.realestate.MqttUnavailableException;
+import no.cantara.realestate.RealEstateException;
 import no.cantara.realestate.observations.ObservationMessage;
 
 import java.util.List;
+
 
 public interface ObservationDistributionClient {
 
@@ -13,7 +16,13 @@ public interface ObservationDistributionClient {
 
     boolean isConnectionEstablished();
 
-    void publish(ObservationMessage message);
+    /**
+     * @param observationMessage the observation to publish
+     * @throws RealEstateException if the connection is not established/stable, or the message cannot be built
+     * @throws MqttUnavailableException if the connection is not established or unstable. Daily Quota limit reached may also be the cause.
+     * The client should call closeConnection on the AzureObservationDistributionClient when receiving this MqttUnavailableException. Then restart the client to re-establish connection to Azure IoT Hub.
+     */
+    void publish(ObservationMessage observationMessage);
 
     long getNumberOfMessagesObserved();
 
